5fb6e426496d20fa8d892a11d730aa24b876d761
22000510 <22000510@handong.ac.kr>
2025-01-07 23:30:05 +0900

    last

--- Assets/Scripts/DongScripts/Timer.cs
+++ Assets/Scripts/DongScripts/Timer.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using UnityEngine.SceneManagement;
 public class Timer : MonoBehaviour
 {
     public TMP_Text minTime;
     public TMP_Text secTime;
+    public ScoreManager scoreManager;
+    public GameObject EndUI;
+    public TMP_Text resultwinner; // 게임 종료시 이긴 팀
+   
     float time = 30; // 제한 시간 120초
     int min, sec;
     
+    int max = 0;
+    int result = 0;
     void Start()
     {
         //제한 시간 02:00
@@ -19,7 +25,7 @@ public class Timer : MonoBehaviour
 
     }
     
-        void Update()
+    void Update()
     { 
         time -= Time.deltaTime;
 
@@ -30,11 +36,27 @@ public class Timer : MonoBehaviour
         {
             minTime.text = 0.ToString();
             secTime.text = 0.ToString();
-
+            for(int i=1;i<=4;i++){
             
+                if(scoreManager.scoreboard[i]>=max){
+                    max = scoreManager.scoreboard[i];
+                    result = i;
+                }
+            } 
+            Time.timeScale = 0;
+            if(result == 1){
+                resultwinner.text = "winner team: Red team";
+            }else if(result == 2){
+                resultwinner.text = "winner team: Blue team";
+            }else if(result == 3){
+                resultwinner.text = "winner team: Green team";
+            }else if(result == 4){
+                resultwinner.text = "winner team: Yellow team";
+            }
+            EndUI.SetActive(true);
         }
-
         else {
+            Time.timeScale = 1;
             if (sec >= 60)
             {
                 min += 1;
@@ -46,7 +68,13 @@ public class Timer : MonoBehaviour
                 secTime.text = sec.ToString();
             }
         }
-
-
    }
+
+   public void OnClickRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
 }
+    
+
